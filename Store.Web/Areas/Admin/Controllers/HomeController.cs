2e1f03aca0d90e0bd0b9275988a14ad7847d952e
yogendra singh <yogendra.ms95@gmail.com>
2017-10-26 20:52:25 +0530

    first commit

--- Store.Web/Areas/Admin/Controllers/HomeController.cs
+++ Store.Web/Areas/Admin/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Store.Web.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Admin/Home
+        // GET: Admin/Home new one
         public ActionResult Index()
         {
             return View();
